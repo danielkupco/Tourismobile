f1e55dece46cd26fb09f71a503d8c26619a6d699
danielkupco <kupcodandroid@gmail.com>
2016-05-09 20:49:41 +0200

    Material design theme and example activities

--- app/src/main/java/rs/ftn/pma/tourismobile/MainActivity.java
+++ app/src/main/java/rs/ftn/pma/tourismobile/MainActivity.java
@@ -1,7 +1,10 @@
 package rs.ftn.pma.tourismobile;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnNavigation = (Button) findViewById(R.id.btnNavigation);
+        btnNavigation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, NavigationActivity.class));
+            }
+        });
+
+        Button btnScroll = (Button) findViewById(R.id.btnScroll);
+        btnScroll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ScrollingActivity.class));
+            }
+        });
     }
 }
